5c8275cccc71eb650ee0257cde0c988fc522134f
Reginaldo Bertoluci Curtio <reginaldobertoluci@gmail.com>
2016-05-11 15:57:21 -0300

    Update PGHandler.php

--- src/PGHandler/PGHandler.php
+++ src/PGHandler/PGHandler.php
@@ -2,35 +2,32 @@
 
 namespace PGHandler;
 
+<?php
+
+namespace Monolog\Handler;
+
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
-
 /**
- * This class is a handler for Monolog, which can be used
- * to write records in a PostgreSQL table
+ * Classe Handler para o Monolog, utilizada para gravar os registros em uam tabela PostgreSQL
  */
 class PGHandler extends AbstractProcessingHandler {
-
     /**
-     * @var bool defines whether the Postgres connection is been initialized
+     * @var bool define se uma conexão foi inicializada
      */
     private $initialized = false;
-
     /**
-     * @var resource
+     * @var Handler da conexão
      */
     protected $connection;
-
     /**
-     * @var string pg statement name
+     * @var string 
      */
     private $statement;
-
     /**
-     * @var string default table name to store the logs in
+     * @var string nome padrão da tabela
      */
-    private $table = 'logs';
-
+    private $table = 'log_erros';
     /**
      * @param resource $connection
      * @param string $table
@@ -39,42 +36,37 @@ class PGHandler extends AbstractProcessingHandler {
      */
     public function __construct($connection, $table, $level = Logger::DEBUG, $bubble = true)
     {
-        if (!is_resource($connection)) {
-            throw new \InvalidArgumentException('A connection must either be a resource.');
-        }
         $this->connection = $connection;
         $this->table = $table;
         parent::__construct($level, $bubble);
     }
-
     /**
-     * Initializes this handler by creating the table if it not exists
+     * Inicializa o handler criando uma tabela caso não exista
      */
     private function initialize() {
-
-        pg_query(
-            $this->connection,
-            'CREATE TABLE IF NOT EXISTS '.$this->table.' ('
+        $this->statement = $this->connection->prepare(
+            'CREATE TABLE IF NOT EXISTS '.$this->table
+            .' ('
             . 'channel varchar(255),'
             . 'level_name varchar(10),'
             . 'message text,'
             . 'context json,'
             . 'extra json,'
-            . 'datetime timestamp'
+            . 'datetime timestamp DEFAULT now()'
             . ')'
-        );
+        );   
 
-        pg_prepare(
-            $this->connection,
-            $this->statement,
-            'INSERT INTO '.$this->table.' (channel, level_name, message, context, extra, datetime) VALUES ($1, $2, $3, $4, $5, $6)'
-        );
+        $this->statement->execute();
 
+        $this->statement = $this->connection->prepare(
+               'INSERT INTO '
+               .$this->table
+               .' (channel, level_name, message, context, extra, datetime) VALUES (:channel, :level_name, :message, :context, :extra, :datetime)'
+        );
         $this->initialized = true;
     }
-
     /**
-     * Writes the record down to the log of the implementing handler
+     * Grava o registro no log através do handler
      *
      * @param  $record[]
      * @return void
@@ -84,7 +76,6 @@ class PGHandler extends AbstractProcessingHandler {
         if (!$this->initialized) {
             $this->initialize();
         }
-
         $content = [
             'channel'    => $record['channel'],
             'level_name' => $record['level_name'],
@@ -93,7 +84,6 @@ class PGHandler extends AbstractProcessingHandler {
             'extra'      => json_encode($record['extra']),
             'datetime'   => $record['datetime']->format('Y-m-d G:i:s'),
         ];
-        pg_get_result($this->connection);
-        pg_send_execute($this->connection, $this->statement, $content);
+        $this->statement->execute($content);
     }
 }
